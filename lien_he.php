6f011f6a4c811f5ada3f36a55f5e0cc52641d059
thamcthph16766 <77135978+thamcthph16766@users.noreply.github.com>
2021-11-24 13:25:43 +0700

    Add files via upload

---
+++ lien_he.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Liên hệ & Hỗ trợ</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="../css/trang_chu.css">
+    <link rel="stylesheet" href="../bootstraps/bootstrap-5.1.3-dist/css/bootstrap.css">
+</head>
+<body>
+    <div class="container">
+        <nav class="navbar navbar-expand-md navbar-light bg-light sticky-top">
+            <div class="container-fluid">
+                <a class="navbar-branch" href="">
+                    <img src="../image/logo.png" alt="" width="100px">
+                </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item px-5"><a class="nav-link" href="">Trang chủ</a></li>
+                        <li class="nav-item px-5"><a class="nav-link" href="">Sản phẩm</a></li>
+                        <li class="nav-item px-5"><a class="nav-link" href="">Tin tức</a></li>
+                        <li class="nav-item px-5"><a class="nav-link" href="">Liên hệ & Hỗ trợ</a></li>
+                    </ul>
+                </div>
+                    <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3">
+                        <input type="search" class="form-control form-control-dark" placeholder="Search..."
+                            aria-label="Search">
+                    </form>
+                    <div class="btn-group" role="group" aria-label="Basic mixed styles example">
+                        <button type="button" class="btn btn-light">Login</button>
+                    </div>
+                    <div class="btn-group" role="group" aria-label="Basic mixed styles example">
+                        <button type="button" class="btn btn-warning">Sign-up</button>
+                    </div>
+            </div>
+        </nav>
+        <div class="row">
+            <div class="col">
+                <img src="../image/lien_he.jpg" alt="" width="600px">
+            </div>
+            <div class="col mt-3">
+                <div class="mb-3">
+                    <label for="exampleFormControlInput1" class="form-label">Fullname</label>
+                    <input type="text" class="form-control"  placeholder="Full name">
+                  </div>
+                <div class="mb-3">
+                    <label for="exampleFormControlInput1" class="form-label">Email address</label>
+                    <input type="email" class="form-control" id="exampleFormControlInput1" rows="3" placeholder="name@example.com">
+                  </div>
+                  <div class="mb-3">
+                    <label for="exampleFormControlTextarea1" class="form-label">Messenger</label>
+                    <textarea class="form-control" id="exampleFormControlTextarea1" rows="8" placeholder="Type your message"></textarea>
+                  </div>
+            </div>
+        </div>
+        <footer class=" footer row row-cols-7 py-5 my-5 border-top">
+            <div class="col-1">
+    
+            </div>
+            <div class="col">
+                <img src="../image/logo.png" alt="" width="200px">
+                <div class="row">
+                    <div class="col"><img src="../image/face.png" alt="" width="30px"></div>
+                    <div class="col"><img src="../image/tw.png" alt="" width="23px"></div>
+                    <div class="col"><img src="../image/ig.png" alt="" width="30px"></div>
+                    <div class="col"><img src="../image/ytb.png" alt="" width="30px"></div>
+                </div>
+            </div>
+    
+            <div class="col">
+    
+            </div>
+    
+            <div class="col">
+                <h5>About Us</h5>
+                <ul class="nav flex-column">
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted"><h6>Address</h6></a></li>
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Store & Office</a></li>
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Jl. Setrasari Kulon III, No. 10-12, </a></li>
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Sukarasa, Sukasari, Bandung, Jawa Barat,</a></li>
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Indonesia 40152</a></li>
+                </ul>
+            </div>
+            <div class="col">
+    
+            </div>
+            <div class="col">
+                <h5>Get in touch</h5>
+                <ul class="nav flex-column">
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted"><h6>Phone    022-20277564</h6></a></li>
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted"><h6>Service     0811-233-8899</h6></a></li>
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Center</a></li>
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted"><h6>Customer     0811-235-9988</h6></a></li>
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Service</a></li>
+                </ul>
+            </div>
+            <div class="col-1">
+    
+            </div>
+        </footer>
+    </div>
+</body>
+</html>
