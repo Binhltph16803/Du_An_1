435517382c132bf67daaf02ac19e758d08cf05d7
thamcthph16766 <77135978+thamcthph16766@users.noreply.github.com>
2021-11-24 10:53:26 +0700

    Add files via upload

--- login.php
+++ login.php
@@ -3,53 +3,45 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Login</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="../css/login.css">
+    <link rel="stylesheet" href="../bootstraps/bootstrap-5.1.3-dist/css/bootstrap.css">
 </head>
 <body>
-    <div class="duan1">
-        <div class="herder">
-            <img src="../image/logo.png" alt="" width="70px">
-        </div>
-        <div class="main">
-            <div class="logo">
-                <img src="../image/logo_login.jpg" alt="" width="800px">
+    <div class="container">
+        <div class="row">
+            <div class="col-7">
+                <img src="../image/logo_login.jpg" alt="" width="750px">
             </div>
-            <div class="login">
-                <p>Webcome back</p>
-                <h1>Login to your account</h1>
-                <div class="thong_tin">
-                    <div class="tt">
-                        <b>Email</b> <br>
-                        <input type="email" placeholder="Email"><br>
+            <div class="col">
+                <p>Welcome back</p>
+                <h3>Login to your account</h3>
+                <br>
+                <form>
+                    <div class="mb-3">
+                      <label for="exampleInputEmail1" class="form-label">Email address</label>
+                      <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email" aria-describedby="emailHelp">
+                      <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
                     </div>
-                    <div class="tt">
-                        <b>Password</b> <br>
-                        <input type="password" placeholder="Mật khẩu"> <br>
+                    <div class="mb-3">
+                      <label for="exampleInputPassword1" class="form-label">Password</label>
+                      <input type="password" class="form-control" id="exampleInputPassword1">
                     </div>
-                    <div class="check">
-                        <div class="real">
-                            <input type="checkbox"> Remember me
-                        </div>
-                        <div class="mk">
-                            <a href="">Forgot password?</a>
-                        </div>
+                    <div class="mb-3 form-check">
+                      <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                      <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                      <a href="">Forgot password?</a>
                     </div>
-                    <div class="button">
-                        <button type="submit"><b>Login now</b></button>
+                    <div class="d-grid gap-2">
+                        <button class="btn btn btn-warning" type="button">Login now</button>
+                        <button class="btn btn btn-dark" type="button"><img src="../image/gg.png" width="20px" alt="">Or sign-in with google</button>
+                      </div>
+                    <div class="d-grid gap-2">
+                        <p>Dont have an account?<a href="">Join free today</a> </p>
                     </div>
-                    <div class="gg">
-                        <img src="../image/gg.png" alt="" width="20px">
-                        <b>Or sign-in with google</b>
-                    </div>
-                    <div class="join">
-                        <a href="">Dont have an account? Join free today</a>
-                    </div>
-                </div>
+                  </form>
             </div>
-        </div>
-        <div class="footer">
-            
         </div>
     </div>
 </body>
